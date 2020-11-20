b66c18416974280f914a0ca82727fcafbcb71f40
Shawn Castrianni <Shawn.Castrianni@ihsmarkit.com>
2020-11-20 10:38:29 -0600

    Change revision to not ForceNew to allow seamless updates Add delay option

--- apigee/resource_proxy_deployment.go
+++ apigee/resource_proxy_deployment.go
@@ -5,11 +5,13 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/go-http-utils/headers"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/scastria/terraform-provider-apigee/apigee/client"
 	"net/http"
+	"net/url"
 	"strconv"
 )
 
@@ -17,6 +19,7 @@ func resourceProxyDeployment() *schema.Resource {
 	return &schema.Resource{
 		CreateContext: resourceProxyDeploymentCreate,
 		ReadContext:   resourceProxyDeploymentRead,
+		UpdateContext: resourceProxyDeploymentUpdate,
 		DeleteContext: resourceProxyDeploymentDelete,
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
@@ -35,11 +38,21 @@ func resourceProxyDeployment() *schema.Resource {
 			"revision": {
 				Type:         schema.TypeInt,
 				Required:     true,
-				ForceNew:     true,
 				ValidateFunc: validation.IntAtLeast(1),
+			},
+			"delay": {
+				Type:             schema.TypeInt,
+				Optional:         true,
+				ValidateFunc:     validation.IntAtLeast(0),
+				DiffSuppressFunc: delayDiff,
 			},
 		},
 	}
+}
+
+func delayDiff(k string, old string, n string, d *schema.ResourceData) bool {
+	//Suppress all diffs
+	return true
 }
 
 func resourceProxyDeploymentCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
@@ -86,6 +99,28 @@ func resourceProxyDeploymentRead(ctx context.Context, d *schema.ResourceData, m 
 	lastRevision := retVal.Revisions[len(retVal.Revisions)-1]
 	revision, _ := strconv.Atoi(lastRevision.Name)
 	d.Set("revision", revision)
+	return diags
+}
+
+func resourceProxyDeploymentUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	envName, proxyName := client.ProxyDeploymentDecodeId(d.Id())
+	c := m.(*client.Client)
+	revision := d.Get("revision").(int)
+	delay := d.Get("delay").(int)
+	requestPath := fmt.Sprintf(client.ProxyDeploymentRevisionPath, c.Organization, envName, proxyName, revision)
+	requestForm := url.Values{
+		"override": []string{strconv.FormatBool(true)},
+		"delay":    []string{strconv.Itoa(delay)},
+	}
+	requestHeaders := http.Header{
+		headers.ContentType: []string{client.FormEncoded},
+	}
+	_, err := c.HttpRequest(http.MethodPost, requestPath, nil, requestHeaders, bytes.NewBufferString(requestForm.Encode()))
+	if err != nil {
+		d.SetId("")
+		return diag.FromErr(err)
+	}
 	return diags
 }
 
