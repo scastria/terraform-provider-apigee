997279ecd649c8e39a9de31c76add8ffa87da2aa
ev-the-dev <evanjhutch@gmail.com>
2023-07-05 16:22:49 +0000

    Squashed commit of the following:
    commit b24cb7cb14fa7b73967326c5472e65e542fb9d26
    Author: Evan Hutch <92749031+ev-the-dev@users.noreply.github.com>
    Date:   Wed Jul 5 12:17:27 2023 -0400
    
        updating target-server docs to include common_name support (#3)
    
    commit aa707e2658427b1e568b6bd434e50e496c7c5340
    Merge: 5574ed5 17ab90a
    Author: Evan Hutch <92749031+ev-the-dev@users.noreply.github.com>
    Date:   Wed Jul 5 10:41:39 2023 -0400
    
        Merge branch 'scastria:main' into main
    
    commit 17ab90a36a5eacdd463c7dae0659f636c2fccc63
    Author: Evan Hutch <92749031+ev-the-dev@users.noreply.github.com>
    Date:   Wed Jul 5 10:36:10 2023 -0400
    
        Adding Support for Target Server's SSL Common Name Field (#60)
    
        * adding support for TLSinfo commonname property on apigee targetserver resource
    
        * changing workflow to run on release instead of tag generation
    
        * Revert "changing workflow to run on release instead of tag generation"
    
        This reverts commit a6158761c9eb311e7d96b45a136889dc220c6b65.
    
        * adding write permissions for release on the github action
    
        * commenting out my added code
    
        * Re-adding my changes and updating some of the default common name logic
    
        * adding an else statement to default the common name value to the Host value
    
    commit 5574ed5c82107cbbef9f2a2814696d7ac78ccc04
    Author: ev-the-dev <evanjhutch@gmail.com>
    Date:   Tue Jul 4 16:14:35 2023 +0000
    
        adding an else statement to default the common name value to the Host value
    
    commit 1f1d8446709335d4fe833bf96460bed8f71e7cc0
    Author: ev-the-dev <evanjhutch@gmail.com>
    Date:   Mon Jul 3 23:04:01 2023 +0000
    
        Re-adding my changes and updating some of the default common name logic
    
    commit 8db5124fb4e7fd713769c361e48d0445f32de169
    Merge: 658db7f 7918a42
    Author: Evan Hutch <92749031+ev-the-dev@users.noreply.github.com>
    Date:   Mon Jul 3 14:38:18 2023 -0400
    
        Merge pull request #2 from ev-the-dev/test/revert
    
        commenting out my added code
    
    commit 7918a42dab1be86671d257bda0e1219371118ecb
    Author: ev-the-dev <evanjhutch@gmail.com>
    Date:   Mon Jul 3 18:37:43 2023 +0000
    
        commenting out my added code
    
    commit 658db7f33dd979383aade237e9a813b7c6da230b
    Author: ev-the-dev <evanjhutch@gmail.com>
    Date:   Tue Jun 27 19:09:33 2023 +0000
    
        adding write permissions for release on the github action
    
    commit 969ac6def1aabd521e5fae9b69c5130bbfb2aa46
    Author: ev-the-dev <evanjhutch@gmail.com>
    Date:   Tue Jun 27 18:31:49 2023 +0000
    
        Revert "changing workflow to run on release instead of tag generation"
    
        This reverts commit a6158761c9eb311e7d96b45a136889dc220c6b65.
    
    commit a6158761c9eb311e7d96b45a136889dc220c6b65
    Author: ev-the-dev <evanjhutch@gmail.com>
    Date:   Tue Jun 27 17:57:17 2023 +0000
    
        changing workflow to run on release instead of tag generation
    
    commit 03a71fb41922858a49738dadf8a9190d5a43b77b
    Merge: 01e5aa9 e705b95
    Author: Evan Hutch <92749031+ev-the-dev@users.noreply.github.com>
    Date:   Mon Jun 26 16:04:49 2023 -0400
    
        Merge pull request #1 from ev-the-dev/feature/targetserver-tlsinfo-commonname
    
        Provider Support for TSLinfo.CommonName Property
    
    commit e705b954f737daf62e1f9d36df6f0048b09bf895
    Author: Evan Hutch <evanjhutch@gmail.com>
    Date:   Fri Jun 23 18:40:22 2023 -0400
    
        adding support for TLSinfo commonname property on apigee targetserver resource

--- apigee/client/ssl_info.go
+++ apigee/client/ssl_info.go
@@ -1,20 +1,27 @@
 package client
 
+type SSLCommonName struct {
+	Value         string `json:"value,omitempty"`
+	WildcardMatch bool   `json:"wildcardMatch,omitempty"`
+}
+
 type SSL struct {
-	Enabled                string   `json:"enabled"`
-	KeyStore               string   `json:"keyStore,omitempty"`
-	KeyAlias               string   `json:"keyAlias,omitempty"`
-	TrustStore             string   `json:"trustStore,omitempty"`
-	ClientAuthEnabled      string   `json:"clientAuthEnabled,omitempty"`
-	IgnoreValidationErrors bool     `json:"ignoreValidationErrors,omitempty"`
-	Protocols              []string `json:"protocols,omitempty"`
+	Enabled                string         `json:"enabled"`
+	KeyStore               string         `json:"keyStore,omitempty"`
+	KeyAlias               string         `json:"keyAlias,omitempty"`
+	TrustStore             string         `json:"trustStore,omitempty"`
+	CommonName             *SSLCommonName `json:"commonName,omitempty"`
+	ClientAuthEnabled      string         `json:"clientAuthEnabled,omitempty"`
+	IgnoreValidationErrors bool           `json:"ignoreValidationErrors,omitempty"`
+	Protocols              []string       `json:"protocols,omitempty"`
 }
 type GoogleSSL struct {
-	Enabled                bool     `json:"enabled"`
-	KeyStore               string   `json:"keyStore,omitempty"`
-	KeyAlias               string   `json:"keyAlias,omitempty"`
-	TrustStore             string   `json:"trustStore,omitempty"`
-	ClientAuthEnabled      bool     `json:"clientAuthEnabled,omitempty"`
-	IgnoreValidationErrors bool     `json:"ignoreValidationErrors,omitempty"`
-	Protocols              []string `json:"protocols,omitempty"`
+	Enabled                bool           `json:"enabled"`
+	KeyStore               string         `json:"keyStore,omitempty"`
+	KeyAlias               string         `json:"keyAlias,omitempty"`
+	TrustStore             string         `json:"trustStore,omitempty"`
+	CommonName             *SSLCommonName `json:"commonName,omitempty"`
+	ClientAuthEnabled      bool           `json:"clientAuthEnabled,omitempty"`
+	IgnoreValidationErrors bool           `json:"ignoreValidationErrors,omitempty"`
+	Protocols              []string       `json:"protocols,omitempty"`
 }
--- docs/resources/target_server.md
+++ docs/resources/target_server.md
@@ -24,6 +24,9 @@ resource "apigee_target_server" "example" {
 * `ssl_truststore` - **(Optional, String)** Name of the truststore that contains the certificate
 * `ssl_client_auth_enabled` - **(Optional, Boolean)** Enable two-way TLS between Apigee and target
 * `ssl_ignore_validation_errors` - **(Optional, Boolean)** Ignore TLS certificate errors
+* `ssl_common_name` - **(Optional, Set)** Set target server's TLS certificate common name attribute. Contains two properties defined below
+    * `value` - **(Optional, String)** The value of the actual target TLS common name of the certificate. Defaults to the host name. Can be set with a `COMMON_NAME` environment variable
+    * `wildcard_match` - **(Optional, Boolean)** Indicates whether the cert should be matched against as a wildcard cert
 * `protocols` - **(Optional, Set)** Specifies the protocols supported by the TargetServer. For example, SSLv3, TLSv1, TLSv1.1, or TLSv1.2. If no protocols are specified, then all protocols available for the JVM will be permitted.
 ## Attribute Reference
 * `id` - Same as `environment_name`:`name`
