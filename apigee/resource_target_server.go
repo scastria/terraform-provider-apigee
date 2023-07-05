17ab90a36a5eacdd463c7dae0659f636c2fccc63
Evan Hutch <92749031+ev-the-dev@users.noreply.github.com>
2023-07-05 10:36:10 -0400

    Adding Support for Target Server's SSL Common Name Field (#60)
    * adding support for TLSinfo commonname property on apigee targetserver resource
    
    * changing workflow to run on release instead of tag generation
    
    * Revert "changing workflow to run on release instead of tag generation"
    
    This reverts commit a6158761c9eb311e7d96b45a136889dc220c6b65.
    
    * adding write permissions for release on the github action
    
    * commenting out my added code
    
    * Re-adding my changes and updating some of the default common name logic
    
    * adding an else statement to default the common name value to the Host value

--- apigee/resource_target_server.go
+++ apigee/resource_target_server.go
@@ -5,13 +5,15 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+
 	"github.com/go-http-utils/headers"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"github.com/scastria/terraform-provider-apigee/apigee/client"
-	"net/http"
-	"strconv"
 )
 
 func resourceTargetServer() *schema.Resource {
@@ -63,6 +65,25 @@ func resourceTargetServer() *schema.Resource {
 			"ssl_truststore": {
 				Type:     schema.TypeString,
 				Optional: true,
+			},
+			"ssl_common_name": {
+				Type:        schema.TypeSet,
+				Optional:    true,
+				MaxItems:    1,
+				DefaultFunc: getDefaultCommonName,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"value": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							DefaultFunc: schema.EnvDefaultFunc("COMMON_NAME", nil),
+						},
+						"wildcard_match": {
+							Type:     schema.TypeBool,
+							Optional: true,
+						},
+					},
+				},
 			},
 			"ssl_client_auth_enabled": {
 				Type:     schema.TypeBool,
@@ -157,6 +178,22 @@ func fillTargetServer(c *client.TargetServer, d *schema.ResourceData) {
 		if ok {
 			c.SSLInfo.TrustStore = sslTrustStore.(string)
 		}
+		sslCommonName, ok := d.GetOk("ssl_common_name.0")
+		if ok {
+			c.SSLInfo.CommonName = sslCommonName.(*client.SSLCommonName)
+
+			value, ok := d.GetOk("ssl_common_name.0.value")
+			if ok {
+				c.SSLInfo.CommonName.Value = value.(string)
+			} else {
+				c.SSLInfo.CommonName.Value = c.Host
+			}
+
+			wildcard_match, ok := d.GetOk("ssl_common_name.0.wildcard_match")
+			if ok {
+				c.SSLInfo.CommonName.WildcardMatch = wildcard_match.(bool)
+			}
+		}
 		protocols, ok := d.GetOk("protocols")
 		if ok {
 			set := protocols.(*schema.Set)
@@ -191,6 +228,22 @@ func fillGoogleTargetServer(c *client.GoogleTargetServer, d *schema.ResourceData
 		if ok {
 			c.SSLInfo.TrustStore = sslTrustStore.(string)
 		}
+		sslCommonName, ok := d.GetOk("ssl_common_name.0")
+		if ok {
+			c.SSLInfo.CommonName = sslCommonName.(*client.SSLCommonName)
+
+			value, ok := d.GetOk("ssl_common_name.0.value")
+			if ok {
+				c.SSLInfo.CommonName.Value = value.(string)
+			} else {
+				c.SSLInfo.CommonName.Value = c.Host
+			}
+
+			wildcard_match, ok := d.GetOk("ssl_common_name.0.wildcard_match")
+			if ok {
+				c.SSLInfo.CommonName.WildcardMatch = wildcard_match.(bool)
+			}
+		}
 		protocols, ok := d.GetOk("protocols")
 		if ok {
 			set := protocols.(*schema.Set)
@@ -201,6 +254,15 @@ func fillGoogleTargetServer(c *client.GoogleTargetServer, d *schema.ResourceData
 		sslIgnoreValidationErrors, ok := d.GetOk("ssl_ignore_validation_errors")
 		c.SSLInfo.IgnoreValidationErrors = sslIgnoreValidationErrors.(bool)
 	}
+}
+
+func getDefaultCommonName() (interface{}, error) {
+	if v := os.Getenv("COMMON_NAME"); v != "" {
+		common_name := &client.SSLCommonName{Value: v}
+		return common_name, nil
+	}
+
+	return nil, nil
 }
 
 func resourceTargetServerRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
@@ -231,6 +293,7 @@ func resourceTargetServerRead(ctx context.Context, d *schema.ResourceData, m int
 	var host, keyStore, keyAlias, trustStore string
 	var port int
 	var isEnabled, hasSSL, sslEnabled, clientAuthEnabled, ignoreValidationErrors bool
+	var commonName *client.SSLCommonName
 	var protocols []string
 	if c.IsGoogle() {
 		ts := retVal.(*client.GoogleTargetServer)
@@ -242,6 +305,7 @@ func resourceTargetServerRead(ctx context.Context, d *schema.ResourceData, m int
 			keyStore = ts.SSLInfo.KeyStore
 			keyAlias = ts.SSLInfo.KeyAlias
 			trustStore = ts.SSLInfo.TrustStore
+			commonName = ts.SSLInfo.CommonName
 			sslEnabled = ts.SSLInfo.Enabled
 			clientAuthEnabled = ts.SSLInfo.ClientAuthEnabled
 			ignoreValidationErrors = ts.SSLInfo.IgnoreValidationErrors
@@ -257,6 +321,7 @@ func resourceTargetServerRead(ctx context.Context, d *schema.ResourceData, m int
 			keyStore = ts.SSLInfo.KeyStore
 			keyAlias = ts.SSLInfo.KeyAlias
 			trustStore = ts.SSLInfo.TrustStore
+			commonName = ts.SSLInfo.CommonName
 			sslEnabledBool, _ := strconv.ParseBool(ts.SSLInfo.Enabled)
 			sslEnabled = sslEnabledBool
 			clientAuthEnabledBool, _ := strconv.ParseBool(ts.SSLInfo.ClientAuthEnabled)
@@ -275,6 +340,7 @@ func resourceTargetServerRead(ctx context.Context, d *schema.ResourceData, m int
 		d.Set("ssl_keystore", keyStore)
 		d.Set("ssl_keyalias", keyAlias)
 		d.Set("ssl_truststore", trustStore)
+		d.Set("ssl_common_name.0", commonName)
 		d.Set("ssl_client_auth_enabled", clientAuthEnabled)
 		d.Set("ssl_ignore_validation_errors", ignoreValidationErrors)
 		d.Set("protocols", protocols)
